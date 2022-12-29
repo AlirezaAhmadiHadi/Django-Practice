9f9b13ddeb82486081d45f0e4c861a2ac710320b
Alireza <Alireza.hadi1308.aah@gmail.com>
2022-12-29 14:47:01 +0330

    Section 16

--- users/models.py
+++ users/models.py
@@ -10,18 +10,32 @@ class Profile(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
     email = models.EmailField(max_length=200, null=True, blank=True)
     username = models.CharField(max_length=200, null=True, blank=True)
+    location = models.CharField(max_length=200, null=True, blank=True)
     short_intro = models.CharField(max_length=200, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     profile_image = models.ImageField(
         null=True, blank=True, upload_to='profiles/', default='profiles/user-default.png')
     social_github = models.CharField(max_length=200, null=True, blank=True)
-    social_twiter = models.CharField(max_length=200, null=True, blank=True)
+    social_twitter = models.CharField(max_length=200, null=True, blank=True)
     social_linkdin = models.CharField(max_length=200, null=True, blank=True)
     social_youtube = models.CharField(max_length=200, null=True, blank=True)
     social_website = models.CharField(max_length=200, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
-    
+
     def __str__(self) -> str:
         return str(self.user.username)
+
+
+class Skill(models.Model):
+    owner = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.name
--- users/views.py
+++ users/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-
+from .models import Profile
 # Create your views here.
 
+
 def profiles(request):
-    content = {}
+    profiles = Profile.objects.all()
+    content = {'profiles': profiles}
     return render(request, 'users/profiles.html', content)
+
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
+    content = {'profile': profile,
+               'topSkills': topSkills,
+               'otherSkills': otherSkills, }
+    return render(request, 'users/user-profile.html', content)

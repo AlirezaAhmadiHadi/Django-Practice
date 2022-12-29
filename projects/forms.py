b261247ff85401d30089bed62a775301334d9621
Alireza <Alireza.hadi1308.aah@gmail.com>
2022-12-29 14:54:24 +0330

    Fix add Project

--- projects/forms.py
+++ projects/forms.py
@@ -6,7 +6,7 @@ from .models import Project
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ['title', 'featured_image', 'description',
+        fields = ['owner', 'title', 'featured_image', 'description',
                   'demo_link', 'source_link', 'tags']
         widgets = {
             'tags': forms.CheckboxSelectMultiple(),
@@ -15,8 +15,9 @@ class ProjectForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(ProjectForm, self).__init__(*args, **kwargs)
 
-        for name,field in self.fields.items():
-            field.widget.attrs.update({'class': 'input input--text', 'placeholder': 'Add text'})
+        for name, field in self.fields.items():
+            field.widget.attrs.update(
+                {'class': 'input input--text', 'placeholder': 'Add text'})
 
         # self.fields['title'].widget.attrs.update(
         #     {'class': 'input input--text', 'placeholder': 'Add Title'})

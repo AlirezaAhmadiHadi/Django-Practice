287289322625dbda819ab9d507de401619ee93a5
Alireza <Alireza.hadi1308.aah@gmail.com>
2022-12-30 14:46:27 +0330

    Section 17 (signals -- post_delete & post_save)

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
-import uuid
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 # Create your models here.
 
 
@@ -25,7 +25,7 @@ class Profile(models.Model):
                           primary_key=True, editable=False)
 
     def __str__(self) -> str:
-        return str(self.user.username)
+        return str(self.username)
 
 
 class Skill(models.Model):

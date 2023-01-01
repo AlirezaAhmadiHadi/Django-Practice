82214c22bab5cadf1b360a55acea43c9d636a43f
Alireza <Alireza.hadi1308.aah@gmail.com>
2023-01-01 11:35:53 +0330

    Section 19 (User Registration)

--- projects/views.py
+++ projects/views.py
@@ -6,50 +6,50 @@ from .forms import ProjectForm
 
 def projects(request):
     projects = Project.objects.all()
-    context = {'projects': projects}
-    return render(request, 'projects/projects.html', context)
+    context = {"projects": projects}
+    return render(request, "projects/projects.html", context)
 
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
-    context = {'project': projectObj}
-    return render(request, 'projects/single-project.html', context)
+    context = {"project": projectObj}
+    return render(request, "projects/single-project.html", context)
 
 
-@login_required(login_url='login')
+@login_required(login_url="login")
 def createProject(request):
     form = ProjectForm()
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('projects')
+            return redirect("projects")
 
-    context = {'form': form}
-    return render(request, 'projects/project_form.html', context)
+    context = {"form": form}
+    return render(request, "projects/project_form.html", context)
 
 
-@login_required(login_url='login')
+@login_required(login_url="login")
 def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid():
             form.save()
-            return redirect('projects')
+            return redirect("projects")
 
-    context = {'form': form}
-    return render(request, 'projects/project_form.html', context)
+    context = {"form": form}
+    return render(request, "projects/project_form.html", context)
 
 
-@login_required(login_url='login')
+@login_required(login_url="login")
 def deleteProject(request, pk):
     project = Project.objects.get(id=pk)
-    if request.method == 'POST':
+    if request.method == "POST":
         project.delete()
-        return redirect('projects')
-    context = {'object': project}
-    return render(request, 'projects/delete_template.html', context)
+        return redirect("projects")
+    context = {"object": project}
+    return render(request, "projects/delete_template.html", context)
--- users/views.py
+++ users/views.py
@@ -2,59 +2,79 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
+from .forms import CustomUserCreationForm
 from .models import Profile
 
 # Create your views here.
 
 
 def loginUser(request):
+    page = "login"
 
     if request.user.is_authenticated:
-        return redirect('profiles')
+        return redirect("profiles")
 
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
 
         try:
             user = User.objects.get(username=username)
         except:
-            messages.error(request, 'Username does not exist')
+            messages.error(request, "Username does not exist")
 
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
             print(user)
             login(request, user)
-            return redirect('profiles')
+            return redirect("profiles")
         else:
-            messages.error(request, 'Username OR Password is incorrect')
+            messages.error(request, "Username OR Password is incorrect")
 
-    return render(request, 'users/login_register.html')
+    context = {"page": page}
+    return render(request, "users/login_register.html", context)
 
 
 def logoutUser(request):
     logout(request)
-    messages.error(request, 'User was logged out!   ')
-    return redirect('login')
+    messages.error(request, "User was logged out!")
+    return redirect("login")
 
 
 def registerUser(request):
-    context = {}
-    return render(request, 'users/login_register.html', context)
+    page = "register"
+    form = CustomUserCreationForm()
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'User account was created!')
+
+            login(request, user)
+
+            return redirect('profiles')
+
+        else:
+            messages.success(request, 'An error has occurred during registration')
+
+    context = {"page": page, "form": form}
+    return render(request, "users/login_register.html", context)
 
 
 def profiles(request):
     profiles = Profile.objects.all()
-    context = {'profiles': profiles}
-    return render(request, 'users/profiles.html', context)
+    context = {"profiles": profiles}
+    return render(request, "users/profiles.html", context)
 
 
 def userProfile(request, pk):
     profile = Profile.objects.get(id=pk)
-    topSkills = profile.skill_set.exclude(description__exact='')
-    otherSkills = profile.skill_set.filter(description='')
-    context = {'profile': profile,
-               'topSkills': topSkills,
-               'otherSkills': otherSkills, }
-    return render(request, 'users/user-profile.html', context)
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
+    context = {"profile": profile,
+               "topSkills": topSkills,
+               "otherSkills": otherSkills, }
+    return render(request, "users/user-profile.html", context)
